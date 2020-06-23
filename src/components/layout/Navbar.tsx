eaf8d7216a090c4f3e25365e0137dcdb6ef94813
MananDesai54 <manan5401desai@gmail.com>
2020-06-23 11:35:27 +0530

    done basic setup with tailwind and storybook

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Navbar:React.FC = () =>{
+    return (
+        <nav className="bg-gray-900 text-white p-5">
+            <span className="text-white-500 text-2xl"><i className="fas fa-book-open"></i> Homework</span>
+        </nav>
+    )
+}
+
+// Navbar.propTypes = {
+
+// }
+
+export default Navbar;
+
---
+++ src/stories/2-navbar.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navbar from '../components/layout/Navbar';
+
+export default {
+    title:'Navbar',
+    component:Navbar
+}
+
+export const navbar = () => <Navbar />
