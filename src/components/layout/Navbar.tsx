c67bf4029ae5bc2734b744cb5cf03671ec298383
MananDesai54 <manan5401desai@gmail.com>
2020-06-23 16:47:40 +0530

    added svg with octicons

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BookIcon } from '@primer/octicons-react'
 // import PropTypes from 'prop-types';
 
 const Navbar:React.FC = () =>{
     return (
         <nav className="bg-gray-900 text-white p-5">
-            <span className="text-white-500 text-2xl"><i className="fas fa-book-open"></i> Homework</span>
+            <span className="text-white-500 text-2xl"><BookIcon size={24}/> Homework</span>
         </nav>
     )
 }
